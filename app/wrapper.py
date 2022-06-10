4f8f46bb8814eb833c64abab4b5e1f530eda0bae
Tristan Cacqueray <tdecacqu@redhat.com>
2022-06-10 22:05:57 +0000

    Update the python wrapper

--- app/wrapper.py
+++ app/wrapper.py
@@ -1,24 +1,27 @@
 #!/bin/python
-# The ansible python module execution wrapper.
-# This code provides a performance boost by keeping the ansible
-# modules code in memory. Though using the python library is not
-# a requirements for Haxible, and the execution could be done
-# using the ansible-playbook command line with a custom callback
-# to record the task results.
+# This module contains two API.
+#
+# - TaskRunner: an interface to run individual individual task.
+# - main: a REPL to receive command from Haxible.
 import sys, json, os
 from ansible.cli import CLI
 from ansible.playbook.play import Play
 from ansible.executor.task_queue_manager import TaskQueueManager
 from ansible.plugins.callback import CallbackBase
 from ansible.plugins.loader import module_loader
+from ansible.template import Templar
+from ansible.vars.hostvars import HostVars
+from ansible.playbook.play_context import PlayContext
+from ansible.executor.play_iterator import PlayIterator
+from ansible.plugins.loader import strategy_loader
+
 
 def loggy(msg):
     if os.environ.get("HAXIBLE_DEBUG"):
         print(f" {os.getpid()} [python] {msg}", file=sys.stderr)
 
-# PlaybookRunner is the ansible abstraction.
-# The run function takes a playbook dictionary and it returns the task result.
-class PlaybookRunner:
+
+class TaskRunner:
     def __init__(self, inventory):
         # A fake cli to setup ansible global context
         class Haxible(CLI):
@@ -38,17 +41,12 @@ class PlaybookRunner:
         self.cli.parse()
         self.loader, self.inventory, self.variable_manager = self.cli._play_prereqs()
 
-    def run(self, playbook):
         # A callback to record task results per hosts.
         class CallbackModule(CallbackBase):
             CALLBACK_VERSION = 2.0
             CALLBACK_TYPE = 'notification'
             CALLBACK_NAME = 'log_plays'
             CALLBACK_NEEDS_WHITELIST = True
-
-            def __init__(self):
-                super(CallbackModule, self).__init__()
-                self.results = dict()
 
             def add_result(self, host, result, ignore_errors=False):
                 # The play only has a single task.
@@ -59,69 +57,123 @@ class PlaybookRunner:
 
             runner_on_ok = add_result
             runner_on_failed = add_result
+        self.cb = CallbackModule()
 
-        cb = CallbackModule()
-        tqm = TaskQueueManager(
+        # The tqm handles the inventory and task execution
+        self.tqm = TaskQueueManager(
             inventory=self.inventory, variable_manager=self.variable_manager,
-            loader=self.loader, passwords=None, forks=5, stdout_callback=cb)
+            loader=self.loader, passwords=None, forks=5, stdout_callback=self.cb)
+        # Pre load some of the tqm components
+        self.tqm.load_callbacks()
+        self.tqm._initialize_processes(5)
+
+        self.templar = Templar(loader=self.loader)
+        self.hostvars = HostVars(
+            inventory=self.inventory,
+            variable_manager=self.variable_manager,
+            loader=self.loader,
+        )
+        self.strategy = strategy_loader.get("linear", self.tqm)
+
+    def run(self, playbook):
+        loggy(f"Running {playbook}")
+        # TODO: handle gather_facts once per session if needed
+        playbook["gather_facts"] = False
         play = Play().load(playbook)
-        run_result = tqm.run(play)
-        if len(cb.results) == 0:
+        # Simulate the tqm.run() function to perform our single task play
+        all_vars = self.variable_manager.get_vars(play=play)
+        self.templar._avaiable_variables = all_vars
+        play_context = PlayContext(play, self.tqm.passwords)
+        iterator = PlayIterator(
+            inventory=self.inventory,
+            play=play,
+            play_context=play_context,
+            variable_manager=self.variable_manager,
+            all_vars=all_vars,
+        )
+        self.cb.results = {}
+        run_result = self.strategy.run(iterator, play_context)
+
+        if len(self.cb.results) == 0:
+            # The run must produce a result.
             print("No result found!", file=sys.stderr)
             exit(1)
-        elif len(cb.results) == 1:
-            task_result = list(cb.results.values())[0]
+        elif len(self.cb.results) == 1:
+            # If the task run on a single host, returns its value directly.
+            task_result = list(self.cb.results.values())[0]
         else:
-            # For multi host results, the output is a Map Host Result with a special key to indicate
-            # the result is for multiple host. Later, the registered value will be substituted back
-            # by the `Haxible.Eval.runTask` function.
-            task_result = {**cb.results, **{"__haxible_multi_hosts": True}}
-        tqm.cleanup()
-        self.loader.cleanup_all_tmp_files()
-        # Returns the code (0 for success, ...) and the result.
-        return [run_result, task_result]
+            # For multi host results, the output is a Map Host Result.
+            # Later, the registered value will be substituted back
+            # using this jinja expension: `{{ registered_result[ansible_host] }}`
+            task_result = {**self.cb.results, **{"__haxible_multi_hosts": True}}
 
-try:
-    (inventory, playPath) = (sys.argv[1], sys.argv[2])
-except IndexError:
-    print("usage: wrapper.py inventory")
-    exit(1)
-runner = PlaybookRunner(inventory)
-# TODO: check if ansible.cfg needs to be supplied.
-paths = set()
-def add_library_path(path):
-    if path not in paths and path and os.path.isdir(path + "/library"):
-        module_loader.add_directory(path + "/library")
-        paths.add(path)
-os.chdir(os.path.dirname(playPath))
-add_library_path(os.path.dirname(playPath))
+        # Returns the code and the result.
+        result = [run_result, task_result]
+        loggy(f"-> {result}")
+        return result
 
-def run_task(inputs):
-    # The Haxl DataSource provides the play without tasks, the task to run
-    # and extra environment vars for the play.
-    [path, play, task, taskVars] = inputs
-    add_library_path(path)
+    async def async_run(self, playbook):
+        return self.run(playbook)
 
-    # Prepare the final play structure.
-    if taskVars:
-        play.setdefault("vars", {})
-        play["vars"].update(taskVars)
-    loggy(f"{json.dumps(play)}: Running task {json.dumps(task)}")
-    play["tasks"] = [task]
-    play["gather_facts"] = "no"
-    # Call ansible-playbook
-    result = runner.run(play.copy())
-    loggy(f"-> {result}")
-    # Record the full play structure in the task for debug purpose.
-    result[1]["__haxible_play"] = play
-    return result
+def test():
+    # Run test using: `python3 -c "import wrapper; wrapper.test()"`
+    os.environ["HAXIBLE_DEBUG"] = "1"
 
-# run_task([dict(hosts="all"), dict(command="echo {{ ansible_host }}"), []])
+    # Create the runner with an empty inventory
+    runner = TaskRunner("")
+    play = dict(hosts="localhost", tasks=[dict(command="echo Hello World")])
+    print(json.dumps(runner.run(play)))
 
-loggy("Runner ready")
-while True:
-    cmd = sys.stdin.readline()
-    if not cmd:
-        break
-    print(json.dumps(run_task(json.loads(cmd))), flush=True)
-loggy("Runner completed")
+    async def main():
+        play = dict(hosts="localhost", tasks=[dict(command="echo Hello World")])
+        async_task = runner.async_run(play)
+        print("Running...")
+        print(await async_task)
+    import asyncio
+    asyncio.run(main())
+
+def main():
+    try:
+        (inventory, playPath) = (sys.argv[1], sys.argv[2])
+    except IndexError:
+        print("usage: wrapper.py inventory playbook")
+        exit(1)
+    runner = TaskRunner(inventory)
+    # TODO: check if ansible.cfg needs to be supplied.
+    paths = set()
+    def add_library_path(path):
+        if path not in paths and path and os.path.isdir(path + "/library"):
+            module_loader.add_directory(path + "/library")
+            paths.add(path)
+    os.chdir(os.path.dirname(playPath))
+    add_library_path(os.path.dirname(playPath))
+
+    def run_task(inputs):
+        # The Haxl DataSource provides the play without tasks, the task to run
+        # and extra environment vars for the play.
+        [path, play, task, taskVars] = inputs
+        add_library_path(path)
+
+        # Prepare the final play structure.
+        if taskVars:
+            play.setdefault("vars", {})
+            play["vars"].update(taskVars)
+        play["tasks"] = [task]
+        # Call ansible-playbook
+        result = runner.run(play.copy())
+        # Record the full play structure in the task for debug purpose.
+        result[1]["__haxible_play"] = play
+        return result
+
+    loggy("Runner ready")
+    while True:
+        cmd = sys.stdin.readline()
+        if not cmd:
+            break
+        print(json.dumps(run_task(json.loads(cmd))), flush=True)
+    loggy("Runner completed")
+    runner.tqm.cleanup()
+    runner.loader.cleanup_all_tmp_files()
+
+if __name__ == "__main__":
+    main()
